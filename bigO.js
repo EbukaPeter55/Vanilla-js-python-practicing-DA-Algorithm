9a828d303afbe9c4757909c73ef066cc1edd6107
ebuka peter <peterdbrainy5@gmail.com>
2022-03-30 16:58:13 +0100

    Add a code to check the time it takes to run the loop. Add a little bigger array: everyone

---
+++ bigO.js
@@ -0,0 +1,18 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 
+'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
+function findNemo(array){
+    // Check for the time taken to perform the loop, or the performance
+    let t0 = performance.now(); //Code runs before the loop
+    for(let i=0; i<array.length; i++){
+        if(array[i] === 'nemo'){
+            console.log('FOUND NEMO');           
+        }
+    }
+    let t1 = performance.now(); //Code runs after the loop
+    console.log('Call to find Nemo took ' + (t1-t0)
+    + ' milliseconds');
+}
+
+findNemo(everyone);
