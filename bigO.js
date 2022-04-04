e8eb1f03fc5e6d88c7a0284535e51f73926fb902
ebuka <peterdbrainy5@gmail.com>
2022-04-04 22:45:10 +0100

    Add a 0(2) constant time type of bigO

--- bigO.js
+++ bigO.js
@@ -1,19 +1,21 @@
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 
-'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(1000).fill('nemo');
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 
+// 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const large = new Array(10000).fill('nemo');
 
-function findNemo(array){
-    // Check for the time taken to perform the loop, or the performance
-    let t0 = performance.now(); //Code runs before the loop
-    for(let i=0; i<array.length; i++){
-        if(array[i] === 'nemo'){
-            console.log('FOUND NEMO');           
-        }
-    }
-    let t1 = performance.now(); //Code runs after the loop
-    console.log('Call to find Nemo took ' + (t1-t0)
-    + ' milliseconds');
+// function findNemo(array){
+//     // Check for the time taken to perform the loop, or the performance
+//     for(let i=0; i<array.length; i++){
+//         if(array[i] === 'nemo'){
+//             console.log('FOUND NEMO');           
+//         }
+//     }
+// }
+
+// findNemo(large);//o(n) --> Linear Time
+const boxes = [0,1,2,3,4,5];
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]); // 0(1)
+    console.log(boxes[1]); // 0(1)
 }
-
-findNemo(large);
+logFirstTwoBoxes(boxes); //0(2) constant time: It is excellent in the graph
